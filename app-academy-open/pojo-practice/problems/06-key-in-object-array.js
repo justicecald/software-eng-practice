1d981decc6fff411f0ac27afbd831464febbc410
Justice Calderon <justicecalderon@j-station.local>
2023-05-18 00:10:26 -0600

    Adding debugging sesh

---
+++ app-academy-open/pojo-practice/problems/06-key-in-object-array.js
@@ -0,0 +1,36 @@
+/***********************************************************************
+Write a function `keyInObjectArray(objArray, keyString)` that takes in an array of 
+objects as the first parameter and a string as the second. The `keyInObjectArray` 
+will return `true` if any of the objects contains the `keyString` as a key within them, and 
+`false` if not.
+
+
+Examples:
+let objArray = [
+  { name: "Rupert" },
+  { age: 42 },
+  { planet: "Earth", system: "Milky Way" }
+];
+
+keyInObjectArray(objArray, 'planet'); // => true
+keyInObjectArray(objArray, 'age'); // => true
+keyInObjectArray(objArray, 'food'); // => false
+keyInObjectArray(objArray, 'animal'); // => false
+
+***********************************************************************/
+
+function keyInObjectArray(objArray, keyString) {
+  // Your code here
+  let found = false;
+
+  objArray.forEach(function(obj) {
+      if (Object.keys(obj).includes(keyString)) {
+          found = true;
+      }
+  });
+
+  return found;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = keyInObjectArray;
